8118e53fa34eaf0e4e20c5cee5592760e6aada60
Tom Harley <trh@st-andrews.ac.uk>
2020-03-08 03:56:42 +0000

    Allow only POST requests to buttons

--- yes.php
+++ yes.php
@@ -1,3 +1,9 @@
 <?php
-    print(exec("sh yes.sh"))
+    if ( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
+        print(exec("sh yes.sh"));
+
+    } else {
+        header('Location: ./index.html');
+        die();
+    }
 ?>
