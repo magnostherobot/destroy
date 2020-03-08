00da23da9931c7ad48d6a0e88162c70c6909eac2
Tom Harley <trh@st-andrews.ac.uk>
2020-03-08 04:51:45 +0000

    Mainly adjusting relative file paths

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,7 @@
-if [ $(gpg --quiet --decrypt result | head -n 1) -eq 1 ]; then
-    echo xargs --arg-file="peers.txt" --max-procs=20 --max-args=1 --replace='{}' \
-        ssh '{}' sh "$PWD/fill.sh"
+if [ $(gpg --quiet --decrypt ../result | head -n 1) -eq 1 ]; then
+    xargs --arg-file="../peers.txt" --max-procs=20 --max-args=1 --replace='{}' \
+        ssh '{}' sh "$PWD/../fill.sh" &
+    echo "it cannot be stopped."
 else
     echo "not time to destroy"
 fi
--- fill.sh
+++ fill.sh
@@ -1,5 +1,5 @@
 for p in $(seq 1 $(nproc)) ; do
-    yes "test" > "/cs/scratch/trh/destroy-fill-$p.txt" &
+    yes "ha" > "/cs/scratch/trh/destroy-fill-$p.txt" &
 done
 
 wait
